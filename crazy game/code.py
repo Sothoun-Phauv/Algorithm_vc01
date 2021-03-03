cda3548fc431a423dba72c977822bb5e1ff19090
Student <student@PNCLB12-09.pnc.lan>
2021-03-03 12:58:35 +0700

    Done

--- crazy game/code.py
+++ crazy game/code.py
@@ -37,6 +37,8 @@ pop = tk.PhotoImage(file = "images/pop.png")
 monster2 = canvas.create_image(600, 50, image = pop)
 tent = tk.PhotoImage(file = "images/tent.png")
 monster3 = canvas.create_image(700, 50, image = tent)
+monsters = [monster1, monster2, monster3]
+
 # create score
 canvas.create_text(50,30, text = "Score: 0", fill = "red",font = 50)
 canvas.create_text(45,60, text = "Life: 3", fill = "red",font = 50)
@@ -63,12 +65,14 @@ def Rockets():
 
 
 def Shoot():
-    global airplane_fly, leftBullet, rightBullet,middleBullet, bullets, Rockets
+    global airplane_fly, leftBullet, rightBullet,middleBullet, bullets, Rockets, listBullets,Scores
     X1 = canvas.coords(airplane_fly)[0]
     Y1 = canvas.coords(airplane_fly)[1]
     leftBullet = canvas.create_image(X1 - 20, Y1 + 120, image = bullets)
     middleBullet = canvas.create_image(X1 +0, Y1 + 90, image = bullets)
     rightBullet = canvas.create_image(X1 + 20, Y1 + 120, image = bullets)
+    listBullets = [leftBullet, middleBullet, rightBullet]
+    Scores += 1
     Rockets()
 
 # function move
@@ -98,6 +102,12 @@ def move() :
     global x,y,monster1
     
     canvas.moveto(monster1,x,y)
+    for bullet in listBullets:
+        m_height = monster.height()
+        m_width = monster.width()
+        if impactBulletMonster(bullet, monster1, m_height, m_width):
+            canvas.delete(monster1)
+            return None
     if y <660 :
         y += 40
         canvas.after(300,lambda:move())
@@ -107,6 +117,12 @@ def move() :
 def move1() :
     global x1,y,monster2
     canvas.moveto(monster2,x1,y)
+    for bullet in listBullets:
+        m_height = monster.height()
+        m_width = monster.width()
+        if impactBulletMonster(bullet, monster2, m_height, m_width):
+            canvas.delete(monster2)
+            return None
     if y <660 :
         y += 40
         canvas.after(500,lambda:move1())
@@ -116,6 +132,12 @@ def move1() :
 def move2() :
     global x,y,monster3
     canvas.moveto(monster3,x2,y)
+    for bullet in listBullets:
+        m_height = monster.height()
+        m_width = monster.width()
+        if impactBulletMonster(bullet, monster3, m_height, m_width):
+            canvas.delete(monster3)
+            return None
     if y <660 :
         y += 40
         canvas.after(800,lambda:move2())
@@ -123,15 +145,18 @@ def move2() :
         y=0
         canvas.after(800,lambda:move2())
 # function damage
-# def impactBullets(bullets,monster):
+def impactBulletMonster(bullet,monstern, m_height, m_width):
+    b_width = bullets.width()
+    b_height = bullets.height()
+    x_m = canvas.coords(monstern)[0]
+    y_m = canvas.coords(monstern)[1]
+    x_b = canvas.coords(bullet)[0]
+    y_b = canvas.coords(bullet)[1]
+    if (x_m-(m_width+b_width)/2 < x_b < x_m+(m_width+b_width)/2) and (y_m-(m_height+b_height)/2 < y_b < y_m+(m_height+b_height)/2):
+        return True
+    else:
+        return False
     
-# if impactBulletsMonster(bullets,monster):
-#     delete monster
-# if not Shootchooting and not impact:
-#     canvas.after(Rockets)
-# else:
-#     canvas.delete
-#     shoot()
 # create sound
 winsound.PlaySound("sound/gameover3",winsound.SND_FILENAME)
 #Function Call
